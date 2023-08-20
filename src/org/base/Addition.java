2bd0fe798c71f594f4cda8e80119338428acc106
Kokila1993 <koki.nataraj@gmail.com>
2023-08-20 18:26:44 +0530

    New Project

---
+++ src/org/base/Addition.java
@@ -0,0 +1,16 @@
+package org.base;
+
+public class Addition {
+	
+	static int a = 70;
+	static int b = 30;
+	static int c;
+	
+	public static void main(String[] args) {
+		
+		c = a+b;
+		
+		System.out.println("The Addtion value : "+c);
+	}
+	
+}
